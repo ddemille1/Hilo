09f9e3a2003ed2ba2d3e00323548b4464f42f1e2
Jonathant4242 <91383892+Jonathant4242@users.noreply.github.com>
2022-01-22 20:56:13 -0800

    John T. I reworked the classes and the main program.

--- Hilo/gameClasses/card.py
+++ Hilo/gameClasses/card.py
@@ -2,14 +2,11 @@ class Card:
     """This is where we create the card object
     
     Attributes:
-        value: This is the face value of the card from 1-13
+        card_value: This is the face value of the card from 1-13
         
     Methods:
-        drawCard: This generates the random value from 1-13
-            return: value
+        value: return the value of the last card drawn. (card_value)
         
-        assignPoints: This assigns 100 points if the player was correct in guessing if the new card was higher or lower than the previous card.     
-            Arguments: are value (from the card's attributes) and is_playing (from the director's attribures)
-
-            return: points
+        draw: This generates the random value from 1-13, stores in card value. 
+            return: card_value
             """
--- Hilo/gameClasses/director.py
+++ Hilo/gameClasses/director.py
@@ -5,12 +5,24 @@ class Director:
         
         is_playing: a boolean variable. True means still playing, False means not playing.
         
-        choice: If the player has chosen the next card to be higher lower than the current card.
+        choice: If the player has chosen the next card to be higher or lower than the current card.
         
+        win: 100 points
+
+        loser: -75 points
+
     Methods:
-        get_input: gets 'is_playing' and 'choice' from user. Gets new card from card object.
+        start_game: Draw inatial card. Display_output.
+        return points
         
-        do_updates: calculates points. Takes, 
-        
+        get_input: gets 'is_playing' and 'choice' from user.
+            return: is_playing
+
+        take_turn: draw a new card, compare to provious card to choice. 
+            return: win or lose
+
+        update_score: updates points from win or lose. 
+
         display_output: displays new card, displays updated points.
+        return: points
         """
